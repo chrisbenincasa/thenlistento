7bc75fb3e6d03a976a4457c1d50b9fbf8613f5dd
Christian Benincasa <chrisbenincasa@gmail.com>
2012-04-24 16:20:51 -0400

    util.js created for temp code cleanup

---
+++ scripts/util.js
@@ -0,0 +1,20 @@
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function getApiKey()
+{
+  return "1a144ff8653821952e65b0cda2fef616"
+}
+
+function getRequestUrl(method)
+{
+  switch(method)
+  {
+    case "tag.gettopartist":
+      return "http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists"
+    case "track.getsimilar":
+      return "http://ws.audioscrobbler.com/2.0/?method=track.getsimilar"
+  }
+}
